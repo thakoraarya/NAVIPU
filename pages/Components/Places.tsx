29e1d7b8b89344b0bede07762f4a31d4afe5f6ac
Shreyans <103171711+mehtashreyans3602@users.noreply.github.com>
2023-09-23 15:06:50 +0530

    coverted to tsx

--- pages/Components/Places.tsx
+++ pages/Components/Places.tsx
@@ -1,6 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import PlaceCards from './atoms/PlaceCards';
-const places = [
+
+interface Place {
+  id: number;
+  PlaceName: string;
+  Latitude: number;
+  Longitude: number;
+}
+
+const places: Place[] = [
   {
     "id": 0,
     "PlaceName": "PIET",
@@ -62,39 +70,32 @@ const places = [
   }
 ];
 
+interface PlacesProps {
+  onCardClick: (latitude: number, longitude: number) => void;
+}
 
-const Places = ({ onCardClick }) => {
-
-  const handleCardClick = (place) => {
+const Places: React.FC<PlacesProps> = ({ onCardClick }) => {
+  const handleCardClick = (place: Place) => {
     // Pass the clicked place's coordinates to the parent component
     onCardClick(place.Latitude, place.Longitude);
   };
+
   return (
-
-
     <div className='overflow-hidden flex flex-col items-center gap-2 w-[15%] md:w-[10%] h-auto rounded-tr-3xl border-l-0 border-b-0  bg-[#fcf9f1] border-[#78786a] border-2 gap-y-6 p-[1.7%] max-h-[800px]'>
       <p className='font-mono font-medium uppercase text-2xl flex items-center'> Places</p>
-      <section className='h-5/6  scroll-smooth rounded-t-md md:rounded-t-xl w-full flex flex-col gap-2 md:gap-4 overflow-y-scroll'>
-
-        {/* here we have to figure it out that how to pass data from here to parent because we cannot store coords in NearbyCard
-the onlcick event cannot be done in this component as it will take anything as a property so we have to findout what to do
-from here things will change a little */}
+      <section className='h-5/6 scroll-smooth rounded-t-md md:rounded-t-xl w-full flex flex-col gap-2 md:gap-4 overflow-y-scroll'>
         {places.map((data) => (
           <PlaceCards
             key={data.id}
             PlaceName={data.PlaceName}
-            setLatitude={data.Latitude}
-            setLongitude={data.Longitude}
+            Latitude={data.Latitude}
+            Longitude={data.Longitude}
             onClick={() => handleCardClick(data)}
           />
         ))}
-
-        {/* <NearbyCard   PlaceName='PIET' setLatitude={setLatitude} setLongitude={setLongitude}/> */}
-        {/* <NearbyCard placename='PIET' lat="22.28878772337861" lng="73.36346800799352" /> */}
-        {/* Add more NearbyCard components here */}
       </section>
     </div>
-  )
+  );
 }
 
-export default Places
+export default Places;
