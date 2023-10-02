cde76777f1244c6b08a40503e6a1b1fd5a3dd004
thakoraarya <aarya191102@gmail.com>
2023-10-02 12:48:22 +0530

    fix changes for conflict

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
-import LandingPage from "@/src/Views/LandingPage";
+import LandingPage from "../src/views/LandingPage";
 import {useUser} from "@auth0/nextjs-auth0/client";
 import View from "@/pages/layouts/View";
 
 const IndexPage = () => {
 
-    const {user, isLoading} = useUser(); // Use useAuth0 hook to access user information
-    return (
-        isLoading ? <div className='w-screen h-screen bg-amber-50 text-center text-8xl'>loading</div> :
-            user ? <View/> : <LandingPage/>
-    )
+  const {user, isLoading} = useUser(); // Use useAuth0 hook to access user information
+  return (
+      isLoading ? <div className='w-screen h-screen bg-amber-50 text-center text-8xl'>loading</div> :
+          user ? <View/> : <LandingPage/>
+  )
 }
 export default IndexPage;
