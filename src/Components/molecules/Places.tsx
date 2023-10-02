757989fadbfae90a1b6f4805c9a3e8e90aa2dd69
thakoraarya <aarya191102@gmail.com>
2023-10-02 13:22:19 +0530

    fix
    Co-Authored-By: Shreyans Mehta <103171711+mehtashreyans3602@users.noreply.github.com>

--- src/Components/molecules/Places.tsx
+++ src/Components/molecules/Places.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import PlaceCard from '../atoms/PlaceCard';
 import {PlaceData, places} from '../atoms/Data';
 import PlaceDetail from "@/src/Components/atoms/PlaceDetail";
+
 interface PlacesProps {
     onCardClick: (latitude: number, longitude: number) => void;
 }
@@ -17,38 +18,32 @@ const Places: React.FC<PlacesProps> = ({onCardClick}) => {
     };
 
 
-    return (
-        <section
+    return (<section
             className='overflow-hidden flex flex-col items-center gap-2 min-w-fit h-auto rounded-tr-3xl border-l-0 border-b-0  bg-[#fcf9f1] border-[#78786a] border-2 gap-y-6 p-[1.7%] max-h-[800px]'>
-            {selectedPlace && (
-                <span
-                    onClick={() => setSelectedPlace(null)}
-                    className="material-symbols-outlined cursor-pointer font-semibold text-xl w-fit"
-                >
+            <div className="font-mono  items-center font-medium uppercase gap-x-4 text-2xl w-min flex">
+                {selectedPlace && (<span
+                        onClick={() => setSelectedPlace(null)}
+                        className="material-symbols-outlined cursor-pointer font-semibold text-xl w-fit"
+                    >
             arrow_back
-          </span>
-            )}            <section
+          </span>)}
+                Nearby
+            </div>
+            <section
                 className='h-auto scroll-smooth rounded-t-md md:rounded-t-xl w-full flex flex-col gap-2 md:gap-4 overflow-y-scroll'>
-                {selectedPlace ?
-                    <PlaceDetail PlaceName={selectedPlace.PlaceName}
-                                 PlaceDescription={selectedPlace.PlaceDescription}
-                                 PlaceImage={selectedPlace.PlaceImage}
-                    />
-                    : (
-                        places.map((data) => (
-                            <PlaceCard
-                                key={data.id}
-                                PlaceImage={data.PlaceImage}
-                                PlaceName={data.PlaceName}
-                                Latitude={data.Latitude}
-                                Longitude={data.Longitude}
-                                onClick={() => handleCardClick(data)}
-                            />
-                        ))
-                    )}
+                {selectedPlace ? <PlaceDetail PlaceName={selectedPlace.PlaceName}
+                                              PlaceDescription={selectedPlace.PlaceDescription}
+                                              PlaceImage={selectedPlace.PlaceImage}
+                /> : (places.map((data) => (<PlaceCard
+                            key={data.id}
+                            PlaceImage={data.PlaceImage}
+                            PlaceName={data.PlaceName}
+                            Latitude={data.Latitude}
+                            Longitude={data.Longitude}
+                            onClick={() => handleCardClick(data)}
+                        />)))}
             </section>
-        </section>
-    );
+        </section>);
 }
 
 export default Places;
