4c9b83282e03b72537af98563b3deee742e64778
Shreyans <103171711+mehtashreyans3602@users.noreply.github.com>
2023-10-06 01:28:16 +0530

    fix

---
+++ src/Components/atoms/AuthButton.tsx
@@ -0,0 +1,14 @@
+"use client"
+// import {useUser} from '@auth0/nextjs-auth0/client';
+import React from "react";
+
+interface AuthButtonProps {
+    AuthPath: any
+    authState: any
+}
+
+const AuthButton: React.FC<AuthButtonProps> = ({AuthPath, authState}) => {
+    return <a href={AuthPath}>{authState}</a>;
+};
+
+export default AuthButton;
