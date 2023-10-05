c1f6fa775e59b2a19115a303eba3ad5e366d8393
Shreyans <103171711+mehtashreyans3602@users.noreply.github.com>
2023-10-05 17:11:20 +0530

    Updated MapView
    Added Current Location Marker

--- src/components/molecules/MapView.tsx
+++ src/components/molecules/MapView.tsx
@@ -11,6 +11,7 @@ const MapView: React.FC<MapViewProps> = ({ MapLat, MapLong }) => {
   const mapContainer = useRef(null);
   const token: string | undefined = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
   const mapRef = useRef<mapboxgl.Map | null>(null); // Reference to the map instance
+  const destinationMarkerRef = useRef<mapboxgl.Marker | null>(null); // Reference to the destination marker
 
   useEffect(() => {
     mapboxgl.accessToken = token || ''; // Replace with your Mapbox access token
@@ -23,14 +24,33 @@ const MapView: React.FC<MapViewProps> = ({ MapLat, MapLong }) => {
         zoom: 15,
         bearing: 0,
         pitch: 0,
-        minZoom:15,
+        minZoom: 15,
       });
 
       // Save the map instance in the ref
       mapRef.current = map;
 
-      // Add a marker to the map
-      new mapboxgl.Marker().setLngLat([MapLong, MapLat]).addTo(map);
+      map.on('load', () => {
+        // Add a marker for the specified coordinates
+        destinationMarkerRef.current = new mapboxgl.Marker().setLngLat([MapLong, MapLat]).addTo(map);
+
+        // Use the Geolocation API to get the user's current location
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+            const currentLocationMarker = new mapboxgl.Marker().setLngLat([longitude, latitude]).addTo(map);
+
+            // Automatically fly to the new location when MapLat or MapLong change
+            mapRef.current.flyTo({
+              center: [MapLong, MapLat],
+              duration: 3000, // Updated duration for a very slow animation
+            });
+          },
+          (error) => {
+            console.error('Error getting current location:', error);
+          }
+        );
+      });
 
       // Clean up when the component unmounts
       return () => {
@@ -38,17 +58,6 @@ const MapView: React.FC<MapViewProps> = ({ MapLat, MapLong }) => {
       };
     }
   }, [MapLat, MapLong, token]);
-
-  // Automatically fly to the new location when MapLat or MapLong change
-  useEffect(() => {
-    if (mapRef.current) {
-      mapRef.current.flyTo({
-        center: [MapLong, MapLat],
-        duration: 3000, // Updated duration for a very slow animation
-
-      });
-    }
-  }, [MapLat, MapLong]);
 
   return <div ref={mapContainer} className='w-full h-full lg:rounded-2xl' />;
 };
